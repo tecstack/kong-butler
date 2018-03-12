b1c49e01e5233c73d415c6767db56e0b208ed151
seethedoor <dai.sheng@139.com>
2018-03-12 10:03:46 +0800

    添加dev-ci-instance文件夹

---
+++ env.conf/dev-ci-instance/config.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin/env python
+import os
+
+# for common using
+SQLALCHEMY_DATABASE_URI = 'mysql://root@127.0.0.1:3306/bulter_dev_ci'
+# SQLALCHEMY_POOL_RECYCLE = 3600
